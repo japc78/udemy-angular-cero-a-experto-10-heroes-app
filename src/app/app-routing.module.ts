93ec1474449d484afbf157db8beb91c2ae3a59fd
japc <japc.grafico@gmail.com>
2021-01-13 15:45:26 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'hero/:id', component: HeroComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'heroes'}
+];
+
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
